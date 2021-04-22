1487af8d81fbac30294a9fbb2367bdf9937cfe44
ernestognw <ernestognw@gmail.com>
2021-04-22 01:01:21 -0500

    Feature: True/False prepared

---
+++ conOSinTilde/TrueFalseControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrueFalseControllerViewController.swift
+//  conOSinTilde
+//
+//  Created by Ernesto García on 22/04/21.
+//
+
+import UIKit
+
+class TrueFalseControllerViewController: UIViewController {
+    var question : String?
+    var word : String?
+    var answer : Bool?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

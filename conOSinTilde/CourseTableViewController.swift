4e4a8940e7f5bae6cb9f4b085d863422dafd4498
Pamela Lozano <pamelalozano16@gmail.com>
2021-05-30 16:55:31 -0500

    cambiar preguntas de plists TF

--- conOSinTilde/CourseTableViewController.swift
+++ conOSinTilde/CourseTableViewController.swift
@@ -13,7 +13,7 @@ class CourseTableViewController: UITableViewController {
     var courses = [
         Course(category: "Agudas", levels: ["Verdadero/Falso", "Selecciona", "Relaciona"]),
         Course(category: "Graves", levels: ["Verdadero/Falso", "Selecciona", "Relaciona"]),
-        Course(category: "Esdrujulas", levels: ["Verdadero/Falsoe", "Selecciona", "Relaciona"])
+        Course(category: "Esdrujulas", levels: ["Verdadero/Falso", "Selecciona", "Relaciona"])
     ]
     
     
